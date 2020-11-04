31a21d9f9ad5e7efe73cf5d0537eee906017ab89
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-11-04 15:22:27 -0500

    added dummy output

--- client/src/views/Calculator/CalculatorOutput.js
+++ client/src/views/Calculator/CalculatorOutput.js
@@ -4,7 +4,18 @@ const CalculatorOutput = (props) => {
     let calculation_inputs = props.location.state.inputs; // Will be here
     console.log(calculation_inputs);
 
-    return <p><u>Outputs:</u></p>
+
+    /* TODO: This is where we would call the function to get the outputs. For now, dummy outputs will be used. */
+    const outputs = calculation_inputs;
+
+    return (
+        <div>
+            <h1>Outputs:</h1>
+            {Object.keys(outputs).map((e) => 
+            <p>{e}: {outputs[e]}</p>)}
+        </div>
+    );
+
 };
 
 export default CalculatorOutput;
