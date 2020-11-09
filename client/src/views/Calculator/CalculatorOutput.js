9b9878f9746c350e04c2261986ad2c74c5360589
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-11-09 12:12:08 -0500

    added more spreadsheet info on the output page

--- client/src/views/Calculator/CalculatorOutput.js
+++ client/src/views/Calculator/CalculatorOutput.js
@@ -6,13 +6,28 @@ const CalculatorOutput = (props) => {
 
 
     /* TODO: This is where we would call the function to get the outputs. For now, dummy outputs will be used. */
-    const outputs = calculation_inputs;
+    const outputs = {};
+
 
     return (
-        <div>
-            <h1>Outputs:</h1>
-            {Object.keys(outputs).map((e) => 
-            <p>{e}: {outputs[e]}</p>)}
+        <div style={{color: "white"}}>
+            <h1>Project Specifications:</h1>
+            {Object.keys(calculation_inputs).map((e) => 
+                <p>{e}: {calculation_inputs[e]}</p>
+            )}
+
+            <h1>Rig Model</h1>
+            <p>This is the recommended rig. You can change the model to assess different pricings.</p>
+            {/* TODO: get recommended rig from function */}
+            <select>
+                <option>Drill #1</option>
+                <option>Drill #2</option>
+                <option>Drill #3</option>
+            </select>
+            
+            <h1>Pricing Information</h1>
+            <p>...</p>
+            {/* TODO get outputs & pricing info from function */}
         </div>
     );
 
