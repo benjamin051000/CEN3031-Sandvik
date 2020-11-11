1b21f8cf28fce2e1f0e32b48cde38d19d69fd762
Caleb O'Malley <cjomalley14@yahoo.com>
2020-11-11 15:50:28 -0500

    Fixed 'example' naming conventions causing server to crash

--- server/config/express.js
+++ server/config/express.js
@@ -3,7 +3,7 @@ const path = require('path'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    exampleRouter = require('../routes/examples.server.routes');
+    exampleRouter = require('../routes/server.routes');
 
 module.exports.init = () => {
     /* 
@@ -11,7 +11,8 @@ module.exports.init = () => {
         - reference README for db uri
     */
     mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
-        useNewUrlParser: true
+				useNewUrlParser: true,
+				useUnifiedTopology: true
     });
     mongoose.set('useCreateIndex', true);
     mongoose.set('useFindAndModify', false);
