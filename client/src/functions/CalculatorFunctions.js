372d84f5db16dbd5db8c03690a2ff1b3d8904536
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-04 17:26:48 -0500

    finished HP_CMS_STD.

--- client/src/functions/CalculatorFunctions.js
+++ client/src/functions/CalculatorFunctions.js
@@ -76,15 +76,15 @@ function HP_CMS_STD_max_fuel_consumption(fuel_burn, engine_hp) {
     return fuel_burn * engine_hp * 0.7457;
 }
 
-function HMP_CMS_STD_fuel_consumption_on_load_per_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) {
+function HP_CMS_STD_fuel_consumption_on_load_per_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) {
     // load_factor is a form input.
     // fuel_burn is based off engine model.
     // est_parasitic_hp is based off of rig model.
-    // total_hp_compressor is the output of HMP_CMS_STD_total_hp_compressor
+    // total_hp_compressor is the output of HP_CMS_STD_total_hp_compressor
     return load_factor * fuel_burn / (0.85 * 1000) * 0.7457 * (est_parasitic_hp + total_hp_compressor);
 }
 
-function HMP_CMS_STD_total_hp_compressor(alt_ambient_pressure, compressor_vol, running_pressure, friction_mult=1.8) {
+function HP_CMS_STD_total_hp_compressor(alt_ambient_pressure, compressor_vol, running_pressure, friction_mult=1.8) {
     // alt_ambient_pressure is the output of HP_CMS_altitude_ambient_pressure.
     // compressor_vol is a form input.
     // running_pressure
@@ -92,7 +92,7 @@ function HMP_CMS_STD_total_hp_compressor(alt_ambient_pressure, compressor_vol, r
     return 144 * 2 * alt_ambient_pressure * compressor_vol * 1.41 / (33000 * 1.41 - 1) * (running_pressure / alt_ambient_pressure) ** (0.41 / 2.82 - 1);
 }
 
-function HMP_CMS_STD_load_factor(ground_conditions) {
+function HP_CMS_STD_load_factor(ground_conditions) {
     // ground_conditions is the output of <TODO>
 
     // switch(ground_conditions) {
@@ -111,83 +111,108 @@ function HMP_CMS_STD_load_factor(ground_conditions) {
     return 1.1 + 0.05 * ground_conditions;
 }
 
-function HMP_CMS_STD_fuel_consumption_off_load_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) {
+function HP_CMS_STD_fuel_consumption_off_load_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) {
     // load_factor is a form input.
     // fuel_burn and engine_hp are based off of engine model.
     // est_parasitic_hp is based off of rig model.
-    // total_hp_compressor is the output of HMP_CMS_STD_total_hp_compressor
+    // total_hp_compressor is the output of HP_CMS_STD_total_hp_compressor
 
     // Note: This formula is functionally identical to the called function, just scaled by 0.7.
-    return HMP_CMS_STD_fuel_consumption_on_load_per_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) * 0.7;
+    return HP_CMS_STD_fuel_consumption_on_load_per_hour(load_factor, fuel_burn, est_parasitic_hp, total_hp_compressor) * 0.7;
 }
 
-function HMP_CMS_STD_avg_fuel_consumption_per_hour(fuel_consumption_on_load_hr, drilling_time_perc, fuel_consumption_off_load_hour, non_drilling_time_perc) {
-    // fuel_consumption_on_load_hr is the output of HMP_CMS_STD_fuel_consumption_on_load_per_hour.
+function HP_CMS_STD_avg_fuel_consumption_per_hour(fuel_consumption_on_load_hr, drilling_time_perc, fuel_consumption_off_load_hour, non_drilling_time_perc) {
+    // fuel_consumption_on_load_hr is the output of HP_CMS_STD_fuel_consumption_on_load_per_hour.
     // drilling_time_perc is a form input.
-    // fuel_consumption_off_load_hour is the output of HMP_CMS_STD_fuel_consumption_off_load_hour.
+    // fuel_consumption_off_load_hour is the output of HP_CMS_STD_fuel_consumption_off_load_hour.
     // non_drilling_time_perc is the output of HP_CMS_non_drilling_time_percent.
     return fuel_consumption_on_load_hr * drilling_time_perc + fuel_consumption_off_load_hour * non_drilling_time_perc;
 }
 
-function HMP_CMS_STD_avg_load_factor(avg_fuel_consumption_per_hour, max_fuel_consumption) {
-    // avg_fuel_consumption_per_hour is the output of HMP_CMS_STD_avg_fuel_consumption_per_hour.
+function HP_CMS_STD_avg_load_factor(avg_fuel_consumption_per_hour, max_fuel_consumption) {
+    // avg_fuel_consumption_per_hour is the output of HP_CMS_STD_avg_fuel_consumption_per_hour.
     // max_fuel_consumption is the output of HP_CMS_STD_max_fuel_consumption.
     return avg_fuel_consumption_per_hour / max_fuel_consumption;
 }
 
-function HMP_CMS_STD_annual_fuel_consumption(avg_fuel_consumption_per_hour, est_hours) {
-    // avg_fuel_consumption_per_hour is the output of HMP_CMS_STD_avg_fuel_consumption_per_hour.
+function HP_CMS_STD_annual_fuel_consumption(avg_fuel_consumption_per_hour, est_hours) {
+    // avg_fuel_consumption_per_hour is the output of HP_CMS_STD_avg_fuel_consumption_per_hour.
     // est_hours is a form input.
     return avg_fuel_consumption_per_hour * est_hours;
 }
 
-function HMP_CMS_STD_daily_fuel_consumption(annual_fuel_consumption) {
-    // annual_fuel_consumption is the output of HMP_CMS_STD_annual_fuel_consumption.
+function HP_CMS_STD_daily_fuel_consumption(annual_fuel_consumption) {
+    // annual_fuel_consumption is the output of HP_CMS_STD_annual_fuel_consumption.
     return annual_fuel_consumption / 365;
 }
 
-function HMP_CMS_STD_endurance_hours(fuel_tank_size, avg_fuel_consumption_per_hour) {
+function HP_CMS_STD_endurance_hours(fuel_tank_size, avg_fuel_consumption_per_hour) {
     // fuel_tank_size is based off the rig model.
-    // avg_fuel_consumption_per_hour is the output of HMP_CMS_STD_avg_fuel_consumption_per_hour.
+    // avg_fuel_consumption_per_hour is the output of HP_CMS_STD_avg_fuel_consumption_per_hour.
     return fuel_tank_size / avg_fuel_consumption_per_hour;
 }
 
-function HMP_CMS_STD_annual_fuel_cost(annual_fuel_consumption, fuel_cost) {
-    // annual_fuel_consumption is the output of HMP_CMS_STD_annual_fuel_consumption.
+function HP_CMS_STD_annual_fuel_cost(annual_fuel_consumption, fuel_cost) {
+    // annual_fuel_consumption is the output of HP_CMS_STD_annual_fuel_consumption.
     // fuel_cost is a form input.
     return annual_fuel_consumption * fuel_cost / 100;
 }
 
-function HMP_CMS_STD_engine_life_estimated(fuel_v_life, avg_fuel_consumption_per_hour) {
+function HP_CMS_STD_engine_life_estimated(fuel_v_life, avg_fuel_consumption_per_hour) {
     // fuel_v_life is based off of engine model.
-    // avg_fuel_consumption_per_hour is the output of HMP_CMS_STD_avg_fuel_consumption_per_hour.
+    // avg_fuel_consumption_per_hour is the output of HP_CMS_STD_avg_fuel_consumption_per_hour.
     return fuel_v_life * 0.264 / avg_fuel_consumption_per_hour;
 }
 
-function HMP_CMS_STD_cost_to_run_engine_annually(engine_rebuild_cost, engine_life_est, est_hours) {
+function HP_CMS_STD_cost_to_run_engine_annually(engine_rebuild_cost, engine_life_est, est_hours) {
     // engine_rebuild_cost
-    // engine_life_est is the output of HMP_CMS_STD_engine_life_estimated.
+    // engine_life_est is the output of HP_CMS_STD_engine_life_estimated.
     // est_hours is a form input.
     return engine_rebuild_cost / engine_life_est * est_hours;
 }
 
-function HMP_CMS_STD_cost_to_run_compressor_annually(compressor_rebuild_cost, engine_life_est, est_hours) {
+function HP_CMS_STD_cost_to_run_compressor_annually(compressor_rebuild_cost, engine_life_est, est_hours) {
     // compressor_rebuild_cost
-    // engine_life_est is the output of HMP_CMS_STD_engine_life_estimated.
+    // engine_life_est is the output of HP_CMS_STD_engine_life_estimated.
     // est_hours is a form input.
     return compressor_rebuild_cost / engine_life_est * est_hours;
 }
 
-function HMP_CMS_STD_carbon_output(avg_fuel_consumption_per_hour, est_hours) {
+function HP_CMS_STD_carbon_output(avg_fuel_consumption_per_hour, est_hours) {
     // (2.683 t/1000l fuel)(2.957ton/264 Gall) was in the name of this spreadsheet cell.
-    // avg_fuel_consumption_per_hour is the output of HMP_CMS_STD_avg_fuel_consumption_per_hour.
+    // avg_fuel_consumption_per_hour is the output of HP_CMS_STD_avg_fuel_consumption_per_hour.
     // est_hours is a form input.
     return avg_fuel_consumption_per_hour * est_hours * 2.957 / 264;
 }
 
-function HMP_CMS_STD_carbon_tax_per_tonne(carbon_output, carbon_tax_dollars_per_tonne) {
-    // carbon_output is the output of HMP_CMS_STD_carbon_output
-    // carbon_tax_dollars_per_tonne is a form input. 
+function HP_CMS_STD_carbon_tax_per_tonne(carbon_output, carbon_tax_dollars_per_tonne) {
+    // carbon_output is the output of HP_CMS_STD_carbon_output
+    // carbon_tax_dollars_per_tonne is a form input.
     return carbon_output * carbon_tax_dollars_per_tonne;
+}
+
+function HP_CMS_STD_cost_per_hour_fuel_engine_compressor(total_saving_with_component_life_inc, est_hours) {
+    // total_saving_with_component_life_inc is the output of HP_CMS_STD_total_saving_with_component_life_increase.
+    // est_hours is a form input.
+    return total_saving_with_component_life_inc / est_hours;
+}
+
+function HP_CMS_STD_cost_before_and_after_total_savings(carbon_tax_dollars_per_tonne, annual_fuel_cost) {
+    // carbon_tax_dollars_per_tonne is a form input.
+    // annual_fuel_cost is the output of HP_CMS_STD_annual_fuel_cost.
+    return carbon_tax_dollars_per_tonne + annual_fuel_cost;
+}
+
+function HP_CMS_STD_total_saving_with_component_life_increase(total_savings, annual_engine_cost, annual_compressor_cost) {
+    // total_savings is the output of HP_CMS_STD_cost_before_and_after_total_savings
+    // annual_engine_cost  is the output of HP_CMS_STD_cost_to_run_engine_annually.
+    // annual_compressor_cost is the output of HP_CMS_STD_cost_to_run_compressor_annually.
+    return cost_before_and_after_total_savings + annual_engine_cost + annual_compressor_cost;
+}
+
+function HP_CMS_STD_cost_per_hour_fuel_engine_compressor(total_savings, est_hours) {
+    // total_savings is the output of HP_CMS_STD_cost_before_and_after_total_savings. 
+    // est_hours is a form input.
+    return total_savings / est_hours;
 }
 
