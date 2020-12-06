4dc11dc057bf89ddb150f2ba68fae6a577563b8f
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-06 00:38:32 -0500

    Updated App to use the new header
    Will change naming conventions in the future

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Dashboard from "./views/Dashboard/Dashboard"
 import NotFound from "./views/NotFound"
@@ -7,16 +8,17 @@ import Calculator from "./views/Calculator/Calculator"
 import CalculatorOutput from './views/Calculator/CalculatorOutput';
 import Settings from "./views/Settings/Settings"
 import History from "./views/History/History"
-
+import NewHeader from "./components/Header/NewHeader"
+import Login from "./views/Login/Login"
 import './App.css';
 
 const App = () => {
   return (
     <div>
       
-      <Header />
+      <NewHeader/>
       <Switch>
-        <Route exact path="/login"  />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/calculator" component={Calculator} />
         <Route exact path="/history" component={History} />
