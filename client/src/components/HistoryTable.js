57bb12389c9065a0f90eae955dc71e869fb65f09
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-06 00:37:03 -0500

    Updated HistoryTable component to include Project Name and Date

--- client/src/components/HistoryTable.js
+++ client/src/components/HistoryTable.js
@@ -9,17 +9,19 @@ import React from "react";
 
 const HistoryTable = (props) => {
 
-    console.log(props.filteredEntries)
+    console.log(props.searchText.length)
+    console.log(props.filteredEntries.length)
     const returnList = 
-        props.filteredEntries.length === 0
+        props.filteredEntries.length === 0 && props.searchText.length === 0
         ? props.data.map((entry)=>{
+            
             return (
                 <tbody>
                     <tr>
-                        <td></td>
+                        <td data-label="Project">{entry.projectName}</td>
                         <td data-label="Company">{entry.company}</td>
                         <td data-label="Client Name">{entry.clientName}</td>
-                        <td></td>
+                        <td data-label="Date">{entry.date}</td>
                         <td class="right aligned">
                         <button class="ui icon button">
                         <i class="chevron circle right icon"></i>
@@ -29,14 +31,28 @@ const HistoryTable = (props) => {
                 </tbody>
             );
         })
-        : props.filteredEntries.map((entry)=>{
+        :
+        (props.filteredEntries.length === 0 && props.searchText.length)
+        ? 
+            <tbody>
+                <tr>
+                    <td data-label="Project">{"No Results Found"}</td>
+                    <td data-label="Company"></td>
+                    <td data-label="Client Name"></td>
+                    <td data-label="Date"></td>
+                    <td class="right aligned"></td>
+                </tr>
+            </tbody>
+        :
+          props.filteredEntries.map((entry)=>{
+            
             return(
                 <tbody>
                     <tr>
-                        <td></td>
+                        <td data-label="Project">{entry.projectName}</td>
                         <td data-label="Company">{entry.company}</td>
                         <td data-label="Client Name">{entry.clientName}</td>
-                        <td></td>
+                        <td data-label="Date">{entry.date}</td>
                         <td class="right aligned">
                         <button class="ui icon button">
                         <i class="chevron circle right icon"></i>
@@ -47,6 +63,7 @@ const HistoryTable = (props) => {
             );
         });
             
+    console.log(returnList)
     return returnList;
 };
 
