ee109fcdf098144518b8fab4a175a6854c179e13
bnguyen121 <44152745+bnguyen121@users.noreply.github.com>
2020-12-08 14:07:10 -0500

    Added functions for DTH, HP_CMS_outputs

--- client/src/functions/FormulaController.js
+++ client/src/functions/FormulaController.js
@@ -1,12 +1,14 @@
 import { drillingCalc } from './formulas/DrillingCalc.js';
-import { rotaryFormulas, rotaryPower, calculator_rotary } from './formulas/RotaryFormulas.js';
-import { HP_CMS_outputs, HP_CMS_STD, HP_CMS_CMS } from './formulas/HP_CMS.js';
-
-/*
-* Inputs to add:
-* Hammer type: M30, M40, etc
-* rpm
-*/
+import {rotaryFormulas, rotaryPower, calculator_rotary} from './formulas/RotaryFormulas.js';
+import {HP_CMS_outputs, HP_CMS_STD, HP_CMS_CMS} from './formulas/HP_CMS.js';
+import {DTH} from './formulas/DTHFormulas.js';
+//Inputs to add:
+ /*
+ * Drilling time percent
+ * rpm
+ * rock_DRI
+ */
+// const subset_of_obj_properties = (({ a, c }) => ({ a, c }))(object);
 
 
 /**
@@ -40,7 +42,7 @@ import { HP_CMS_outputs, HP_CMS_STD, HP_CMS_CMS } from './formulas/HP_CMS.js';
 export default function run_calculations(input) {
     let rig_model = get_rig_model();
 
-    let drillingCalc_inputs = (({ holeDepth, pulldown }) => ({ holeDepth, pulldown }))(input);
+    let drillingCalc_inputs = (({ holeDepth, rotPulldown }) => ({ holeDepth, rotPulldown }))(input);
     let drillingCalc_outputs = get_drillingCalc_info(drillingCalc_inputs, rig_model);
 
     // Rotary formula calculations
@@ -48,12 +50,29 @@ export default function run_calculations(input) {
     let rotaryFormulas = get_rotaryFormulas_info(input, rig_model, drillingCalc_outputs);
     let rotaryPower = get_rotaryPower_info(input);
 
+    let drillingCalc_inputs = (({holeDepth, pulldown}) => ({holeDepth, pulldown}))(input);
+    let drillingCalc_outputs = get_drillingCalc_info(drillingCalc_inputs, get_rig_model()); 
+
+    let rotaryFormulas_inputs = (({rock_UCS, rpm, holeDepth, fracturization}) => ({rock_UCS, rpm, holeDepth, fracturization}))(input_json);
+    let rotaryFormulas = get_rotaryFormulas_info(input, get_rig_model(), drillingCalc_outputs);
+
+    let rotaryPower_inputs = (({rotary_bit, adjusted_WOB, rpm, bit}) => ({rotary_bit, adjusted_WOB, rpm, bit}))(input_json);
+    let rotaryPower = get_rotaryPower_info(input);
+
+
     return {
         drillingCalc_outputs,
         rotary_info,
         rotaryFormulas,
         rotaryPower
     };
+
+    
+
+
+    return {error: 'NOT IMPLEMENTED'};
+
+    
 }
 
 
@@ -74,7 +93,7 @@ function get_rig_model() {
  */
 function get_drillingCalc_info(input, rig_model) {
     let holeDepth = input.holeDepth;
-    let pulldown = input.pulldown;
+    let pulldown = input.rotPulldown;
 
     let single_pass = rig_model.RHT_SinglePass;
     let pipeLength = rig_model.RHT_PipeLength;
@@ -139,8 +158,8 @@ function get_rotaryFormulas_info(input, rig_model, drillingCalc) {
     let rock_UCS = input.rock_UCS;
     let rpm = input.rpm;
     let holeDepth = input.holeDepth;
-    let fracturization = input.fracturization;   //May want to make a function to calculate it, right now it would be: none. light, moderate, or heavy
-
+    let fracturization_Word = input.fracturization;   
+    let fracturization = DTH.fracturization(fracturization_Word);
     let adjusted_WOB = drillingCalc.adjusted_WOB;
     let pipeLength = rig_model.RHT_PipeLength;
     let feed_rate = rig_model.RHT_FeedRate;
@@ -200,5 +219,126 @@ function get_rotaryPower_info(inputs) {
 
     let rotation_power = rotaryPower.rotation_power(hp);
 
-    return { w, hp, rp: rotation_power };
+    return {w, hp, rp: rotation_power};
 }
+
+function get_HP_CMS_outputs(inputs, rig_model){
+    let drill_time_percent = inputs.drill_time_percent;
+    let altitude = inputs.elevation;
+    let compressor_vol = inputs.dthComp;
+    let hole_diam = rig_model.Rotary_BitSize / 25.4;
+    let rod_diam = rig_model.PipeSize / 25.4;
+
+    let non_drill_time_percent = HP_CMS_outputs.non_drilling_time_percent(drill_time_percent);
+
+    let altitude_ambient_pressure = HP_CMS_outputs.altitude_ambient_pressure(altitude);
+
+    let altitude_volume_mass = HP_CMS_outputs.altitude_volume_mass(compressor_vol, altitude_ambient_pressure);
+
+    let compressor_actual_volume_for_altitude = HP_CMS_outputs.compressor_actual_volume_for_altitude(compressor_vol, altitude_volume_mass);
+
+    let calculated_UHV_with_altitude_derated_volume = HP_CMS_outputs.calculated_UHV_with_altitude_derated_volume(compressor_actual_volume_for_altitude, hole_diam, rod_diam );
+
+    let calculated_volume_altitude_derated_percent = HP_CMS_outputs.calculated_volume_altitude_derated_percent(compressor_actual_volume_for_altitude, compressor_vol);
+
+    return{ non_drill_time_percent,
+        altitude_ambient_pressure,
+        altitude_volume_mass,
+        compressor_actual_volume_for_altitude,
+        calculated_UHV_with_altitude_derated_volume,
+        calculated_volume_altitude_derated_percent
+    };
+}
+
+function get_HP_CMS_STD_outputs(inputs, rig_model){
+
+    let compressor_vol = inputs.dthComp;
+    let altitude_ambient_pressure = inputs.altitude_ambient_pressure //Could use get_HP_CMS_outputs as another parameter to get this
+    let running_pressure = inputs.Wap;
+
+
+
+    //For all Engine values, will use first engine as temp values
+    let fuel_burn = rig_model.Engine[0].Fuel_BurnRate;
+    let engine_hp = rig_model.Engine[0].Nominal_HP;
+    let est_parasitic_hp = 225; //Some rig model has different values but some do not have this maybe just take average value?
+
+    let max_fuel_consumption = HP_CMS_STD.max_fuel_consumption(fuel_burn, engine_hp);
+
+    let total_HP_compressor = HP_CMS_STD.total_hp_compressor(altitude_ambient_pressure, compressor_vol, running_pressure, 1.8);
+    let load_factor = HP_CMS_STD.load_factor();
+
+    let fuel_consumption_on_load_per_hour = HP_CMS_STD.fuel_consumption_on_load_per_hour(load_factor,fuel_burn,est_parasitic_hp,total_HP_compressor);
+
+
+
+}
+
+
+function get_DTH_info(inputs, rig_model, drillingCalc_outputs){
+    
+    let rock_UCS = input.rock_UCS;
+    let holeDepth = input.holeDepth;
+    let fracturization_Word = input.fracturization;   
+    let fracturization = DTH.fracturization(fracturization_Word);
+    let WAP = inputs.dthWap;
+
+
+    let pipeLength = rig_model.RHT_PipeLength;
+    let feed_rate = rig_model.RHT_FeedRate;
+    let hoist_rate = rig_model.RHT_HoistRate;
+    let setup = rig_model.RHT_SetUp;
+    let single_pass = rig_model.RHT_SinglePass;
+    let hammer_ROP_factor1 = rig_model.DTH_Hammer[0].ROP_Factor[0];
+    let hammer_ROP_factor2 = rig_model.DTH_Hammer[0].ROP_Factor[1];
+    let hammer_bit = inputs.dthBit;
+    let rock_DRI = inputs.rock_DRI
+
+    let number_of_pipes = drillingCalc.number_of_pipes(holeDepth, single_pass, pipeLength);
+
+    let ground_conditions = DTH.ground_conditions(rock_UCS);
+
+
+    let adjusted_hammer_ROP = DTH.adjusted_hammer_ROP(hammer_ROP_factor1, hammer_ROP_factor2, hammer_bit);
+
+    let ROP_in_DRI_at_given_pressure = DTH.ROP_in_DRI_at_given_pressure(adjusted_hammer_ROP, WAP);
+
+    let inst_rop = DTH.inst_rop(rock_DRI,ground_conditions,ROP_in_DRI_at_given_pressure);
+
+    let penetration_rate = DTH.penetration_rate(inst_rop);
+
+    let pure_penetration_rate = DTH.pure_penetration_rate(penetration_rate);
+
+    let _80_percent_driller_efficiency_penetration_rate = rotaryFormulas._80_percent_driller_efficiency_penetration_rate(pure_penetration_rate,fracturization);
+
+    let drill_time = rotaryFormulas.drill_time(holeDepth, _80_percent_driller_efficiency_penetration_rate);
+
+    let add_pipe = rotaryFormulas.add_pipe(number_of_pipes, rig_model.RHT_AddPipe);
+
+    let retract = rotaryFormulas.retract(number_of_pipes, pipeLength, feed_rate, hoist_rate);
+
+    let remove = add_pipe;
+
+    let collaring = rotaryFormulas.collaring(fracturization, add_pipe, retract, remove, setup);
+
+    let cleaning = rotaryFormulas.cleaning(fracturization, drill_time);
+
+    //Unsure if we should use the formula made or just add the values
+    let total_time = drill_time + collaring + add_pipe + retract + remove + setup + cleaning;
+
+    let total_percent_time_drilling = drill_time / total_time;
+
+    let net_penetration_rate = rotaryFormulas.net_penetration_rate(_80_percent_driller_efficiency_penetration_rate, total_percent_time_drilling);
+
+    return{
+        drill_time,
+        collaring,
+        add_pipe,
+        retract,
+        setup,
+        cleaning
+    };
+
+}
+
+
