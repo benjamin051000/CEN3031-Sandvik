59b7f379d6ef8518b97b341545e0d52efc0dde9d
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-08 13:10:00 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Route, Switch, Redirect  } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from "./views/Dashboard/Dashboard"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
@@ -8,9 +8,8 @@ import Calculator from "./views/Calculator/Calculator"
 import CalculatorOutput from './views/Calculator/CalculatorOutput';
 import Settings from "./views/Settings/Settings"
 import History from "./views/History/History"
-import NewHeader from "./components/Header/NewHeader"
 import Login from "./views/Login/Login"
-import {getData} from './views/History/HistoryData'
+import { getData } from './views/History/HistoryData'
 import './App.css';
 
 const App = () => {
@@ -21,24 +20,41 @@ const App = () => {
   const [comingFromHistory, setComingFromHistory] = useState([]);
   const [accountId, setAccountId] = useState([]);
   const [isAdmin, setIsAdmin] = useState([]);
+  const [isSignedIn, setIsSignedIn] = useState({
+    name: null
+  });
 
 
 
   return (
     <div>
-      
-      <NewHeader/>
+
+      <Header />
       <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/login" component={Login}
+          setIsAdmin={setIsAdmin}
+          setAccountId={setAccountId}
+          setIsSignedIn={setIsSignedIn}
+        />
+        <Route exact path="/dashboard" component={Dashboard} 
+          isSignedIn = {isSignedIn}
+        />
         <Route exact path="/calculator" component={Calculator} />
-        <Route exact path="/history" component={History} />
-        <Route exact path="/settings" component={Settings} />
-        <Route path='/CalculatorOutput' component={CalculatorOutput} />
+        <Route exact path="/history" component={History}
+          accountId = {accountId}
+          setComingFromHistory={setComingFromHistory}
+        />
+        <Route exact path="/settings" component={Settings}
+          isAdmin={isAdmin}
+        />
+        <Route path='/CalculatorOutput' component={CalculatorOutput}
+          accountId={accountId}
+          comingFromHistory={comingFromHistory}
+        />
         <Route exact path="/">
           <Redirect to="/dashboard" />
         </Route>
-        <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
