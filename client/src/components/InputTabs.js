61ed5a49766065a8c994e1cba79878445391fe74
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-08 14:31:40 -0500

    changed numerical input types to "number"

--- client/src/components/InputTabs.js
+++ client/src/components/InputTabs.js
@@ -38,7 +38,7 @@ const panes = [
                 <div className="input-grid">
                     <p>
                         <label htmlFor="ucs">Rock UCS</label>
-                        <Field name="ucs" type="text" placeholder="In units" />
+                        <Field name="ucs" type="number" placeholder="In units" />
                         <ErrorMessage component="required-message" name="ucs" />
                     </p>
                     <p>
@@ -53,47 +53,47 @@ const panes = [
 
                     <p>
                         <label htmlFor="elevation">Elevation</label>
-                        <Field name="elevation" type="text" placeholder="Feet" />
+                        <Field name="elevation" type="number" placeholder="Feet" />
                         <ErrorMessage component="required-message" name="elevation" />
                     </p>
                     <p>
                         <label htmlFor="temp">Ambient Temperature</label>
-                        <Field name="temp" type="text" placeholder="Degrees in F°" />
+                        <Field name="temp" type="number" placeholder="Degrees in F°" />
                         <ErrorMessage component="required-message" name="temp" />
                     </p>
                     <p>
                         <label htmlFor="fuelCost">Fuel Cost</label>
-                        <Field name="fuelCost" type="text" placeholder="In USD" />
+                        <Field name="fuelCost" type="number" placeholder="In USD" />
                         <ErrorMessage component="required-message" name="fuelCost" />
                     </p>
                     <p>
                         <label htmlFor="estHours">Estimated Hours</label>
-                        <Field name="estHours" type="text" placeholder="Hours" />
+                        <Field name="estHours" type="number" placeholder="Hours" />
                         <ErrorMessage component="required-message" name="estHours" />
                     </p>
                     <p>
                         <label htmlFor="carbTaxTonne">Carbon Tax</label>
-                        <Field name="carbTaxTonne" type="text" placeholder="$/Tonne" />
+                        <Field name="carbTaxTonne" type="number" placeholder="$/Tonne" />
                         <ErrorMessage component="required-message" name="carbTaxTonne" />
                     </p>
                     <p>
                         <label htmlFor="drillTimePercent">Drill Time</label>
-                        <Field name="drillTimePercent" type="text" placeholder="Time %" />
+                        <Field name="drillTimePercent" type="number" placeholder="Time %" />
                         <ErrorMessage component="required-message" name="drillTimePercent" />
                     </p>
                     <p>
                         <label htmlFor="fuelTankSize">Fuel Tank Size</label>
-                        <Field name="fuelTankSize" type="text" placeholder="Gallons" />
+                        <Field name="fuelTankSize" type="number" placeholder="Gallons" />
                         <ErrorMessage component="required-message" name="fuelTankSize" />
                     </p>
                     <p>
                         <label htmlFor="engineRebuildCost">Engine Rebuild Cost</label>
-                        <Field name="engineRebuildCost" type="text" placeholder="Price in USD" />
+                        <Field name="engineRebuildCost" type="number" placeholder="Price in USD" />
                         <ErrorMessage component="required-message" name="engineRebuildCost" />
                     </p>
                     <p>
                         <label htmlFor="compRebuildCost">Compressor Rebuild Cost</label>
-                        <Field name="compRebuildCost" type="text" placeholder="Price in USD" />
+                        <Field name="compRebuildCost" type="number" placeholder="Price in USD" />
                         <ErrorMessage component="required-message" name="compRebuildCost" />
                     </p>
 
@@ -107,13 +107,13 @@ const panes = [
                 <div className="input-grid">
                     <p>
                         <label htmlFor="pipeSize">Pipe Size</label>
-                        <Field name="pipeSize" type="text" placeholder="Diameter of Pipe" />
+                        <Field name="pipeSize" type="number" placeholder="Diameter of Pipe" />
                         <ErrorMessage component="required-message" name="pipeSize" />
                     </p>
 
                     <p>
                         <label htmlFor="holeDepth">Hole Depth</label>
-                        <Field name="holeDepth" type="text" placeholder="Depth in Feet" />
+                        <Field name="holeDepth" type="number" placeholder="Depth in Feet" />
                         <ErrorMessage component="required-message" name="holeDepth" />
                     </p>
                 </div>
@@ -128,13 +128,13 @@ const panes = [
                 <div className="input-grid">
                     <p>
                         <label htmlFor="dthComp">Compressor</label>
-                        <Field name="dthComp" type="text" placeholder="Compressor" />
+                        <Field name="dthComp" type="number" placeholder="Compressor" />
                         <ErrorMessage component="required-message" name="dthComp" />
                     </p>
 
                     <p>
                         <label htmlFor="dthWap">WAP</label>
-                        <Field name="dthWap" type="text" placeholder="WAP" />
+                        <Field name="dthWap" type="number" placeholder="WAP" />
                         <ErrorMessage component="required-message" name="dthWap" />
                     </p>
 
@@ -146,7 +146,7 @@ const panes = [
 
                     <p>
                         <label htmlFor="dthBit">Bit</label>
-                        <Field name="dthBit" type="text" placeholder="Drill Bit" />
+                        <Field name="dthBit" type="number" placeholder="Drill Bit" />
                         <ErrorMessage component="required-message" name="dthBit" />
                     </p>
                     <p>
@@ -166,23 +166,23 @@ const panes = [
                 <div className="input-grid">
                     <p>
                         <label htmlFor="rotPulldown">Pulldown</label>
-                        <Field name="rotPulldown" type="text" placeholder="Pull Down" />
+                        <Field name="rotPulldown" type="number" placeholder="Pull Down" />
                         <ErrorMessage component="required-message" name="rotPulldown" />
                     </p>
                     <p>
                         <label htmlFor="rotComp">Compressor</label>
-                        <Field name="rotComp" type="text" placeholder="Compressor Type" />
+                        <Field name="rotComp" type="number" placeholder="Compressor Type" />
                         <ErrorMessage component="required-message" name="rotComp" />
                     </p>
                     <p>
                         <label htmlFor="rotBit">Bit</label>
-                        <Field name="rotBit" type="text" placeholder="Rotary Bit" />
+                        <Field name="rotBit" type="number" placeholder="Rotary Bit" />
                         <ErrorMessage component="required-message" name="rotBit" />
                     </p>
                     <p>
                         {/* TODO: Consider a tooltip that says "Revolutions per minute" or additional info */}
                         <label htmlFor="rotRpm">RPM</label>
-                        <Field name="rotRpm" type="text" placeholder="Rotations per Minute" />
+                        <Field name="rotRpm" type="number" placeholder="Rotations per Minute" />
                         <ErrorMessage component="required-message" name="rotRpm" />
                     </p>
                 </div>
