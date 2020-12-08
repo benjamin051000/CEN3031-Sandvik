3aaebbad748808484dfad0add899b7fdebe89117
bnguyen121 <44152745+bnguyen121@users.noreply.github.com>
2020-12-07 20:24:42 -0500

    Finished up get_drillingCalc_information

--- client/src/functions/FormulaController.js
+++ client/src/functions/FormulaController.js
@@ -26,6 +26,8 @@ import {HP_CMS_outputs, HP_CMS_STD, HP_CMS_CMS} from './formulas/HP_CMS.js';
     rotBit: '',
     rotRpm: ''
 } */
+//Inputs to add:
+// Hammer type: M30, M40, etc
 
 // const subset_of_obj_properties = (({ a, c }) => ({ a, c }))(object);
 
@@ -34,6 +36,8 @@ export default function run_calculations(input_json) {
 
     let rotary_output = get_rotary_information(rotary_inputs, get_rig_model());
 
+    let drillingCalc_inputs = (({holeDepth, pulldown}) => ({holeDepth, pulldown}))(input_json);
+    let drillingCalc_outputs = get_drillingCalc_information(drillingCalc_inputs, get_rig_model()); 
     return {error: 'NOT IMPLEMENTED'};
 }
 
@@ -44,6 +48,8 @@ function get_rig_model() {
 
 function get_drillingCalc_information(input, rig_model){
     let holeDepth = input.holeDepth;  //input
+    let pulldown = input.pulldown; //input
+
     let single_pass = rig_model.single_pass;
     let pipeLength = rig_model.pipeLength;
     let loader_cap = rig_model.loader_cap;
@@ -52,8 +58,24 @@ function get_drillingCalc_information(input, rig_model){
 
     let number_of_pipes = drillingCalc.number_of_pipes(holeDepth, single_pass, pipeLength);
     let number_of_pipes_too_deep = drillingCalc.number_of_pipes_too_deep(holeDepth, single_pass, pipeLength, loader_cap, number_of_pipes);
-    let drill_string_wt = drillingCalc.get_drill_string_wt(loader_cap,pipeWeight)
 
+    let pipeWeight = drillingCalc.get_pipe_weight(pipeLength);
+    let drill_string_wt = drillingCalc.get_drill_string_wt(loader_cap, pipeWeight, number_of_pipes_too_deep);
+    let available_WOB = drillingCalc.available_WOB(rig_model.RHT_RHWeight,rig_model.RPD_MaxPulldown, drill_string_wt);
+
+    let pulldown_Force = drillingCalc.get_pulldown_force(rig_model.RPD_MaxPulldown, rig_model.RPD_MaxFeedPressure, pulldown)
+    let adjusted_WOB = drillingCalc.adjusted_WOB(rig_model.RHT_RHWeight,pulldown_Force, drill_string_wt);
+
+    let adjusted_feed_pressure = drillingCalc.get_adjusted_feed_pressure(hammer);
+    let pulldown_force_DTH = drillingCalc.get_pulldown_force_DTH(adjusted_feed_pressure,pulldown);
+    let adjusted_WOB_for_DTH = drillingCalc.adjusted_WOB_for_DTH(rig_model.RHT_RHWeight,drill_string_wt, pulldown_force_DTH);
+
+    return {
+        available_WOB,
+        adjusted_WOB,
+        adjusted_WOB_for_DTH
+
+    };
 }
 
 /**
