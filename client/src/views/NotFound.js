53c9286aef919c20213f1ef1da791d3ca99db9c4
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-07 23:32:23 -0500

    Update NotFound.js

--- client/src/views/NotFound.js
+++ client/src/views/NotFound.js
@@ -1,5 +1,11 @@
+import React from 'react';
 const NotFound = () => {
-        return "Page not found"
+        return (
+                <div>
+                        <div style={{fontSize:"24pt", color:"#009aff"}} class="ui centered header">Page not found</div>
+                        <img style={{border:"4px solid #009aff"}} class="ui centered image" src={ "/sandviklogo.png" } alt="Sandvik logo" />
+                </div>
+        );
 }
 
 export default NotFound;
