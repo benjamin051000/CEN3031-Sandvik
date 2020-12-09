35598f0fa843bbc05304c01680d93abaa88a78fa
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-09 11:46:56 -0500

    Updated state logic

--- client/src/views/History/History.js
+++ client/src/views/History/History.js
@@ -1,52 +1,53 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HistoryTable from "../../components/HistoryTable"
 import Search from "../../components/Search"
-import {getData} from './HistoryData'
+import { getData } from './HistoryData'
 import './History.css'
 
-function History(props) {
+function History() {
 
-    const [data, setData] = useState(getData());
-    const [filteredEntries, setFilteredEntries] = useState([]);
-    const [searchText, setSearchText] = useState([]);
+  localStorage.setItem("fromHistory", true)
 
-    return (
-        <div>
-          <div class="ui centered container">
-            <h1 style={{color:"#009aff", fontSize:"26pt", marginBottom:"15px"}} class="ui header">History</h1>
-            <Search 
-              data={data} 
-              setFilteredEntries={setFilteredEntries} 
-              setSearchText={setSearchText}
+  const data = getData();
+  const [filteredEntries, setFilteredEntries] = useState([]);
+  const [searchText, setSearchText] = useState([]);
+
+  return (
+    <div>
+      <div class="ui centered container">
+        <h1 style={{ color: "#009aff", fontSize: "26pt", marginBottom: "15px" }} class="ui header">History</h1>
+        <Search
+          data={data}
+          setFilteredEntries={setFilteredEntries}
+          setSearchText={setSearchText}
+        />
+
+        <div
+          class="table-responsive"
+          className="table-wrapper"
+        >
+          <table class="ui inverted stackable single line fixed striped selectable table padded">
+            <thead>
+              <tr>
+                <th>Project</th>
+                <th>Company</th>
+                <th>Client Name</th>
+                <th>Date</th>
+                <th class="two wide" colSpan="1"></th>
+              </tr>
+            </thead>
+            <HistoryTable
+              data={data}
+              filteredEntries={filteredEntries}
+              searchText={searchText}
             />
-            
-            <div
-                class="table-responsive"
-                className="table-wrapper"
-
-              >
-                <table class="ui inverted stackable single line fixed striped selectable table padded">
-                  <thead>
-                    <tr>
-                      <th>Project</th>
-                      <th>Company</th>
-                      <th>Client Name</th>
-                      <th>Date</th>
-                      <th class="two wide" colSpan="1"></th>
-                    </tr>
-                  </thead>
-                  <HistoryTable
-                    data={data}
-                    filteredEntries={filteredEntries}
-                    searchText={searchText}
-                  />
-                </table>
-            </div>
-            <Link to="/dashboard" style={{margin:"20px 0 40px 0"}} class="ui blue huge padded button" >Back</Link>
-          </div>
+          </table>
         </div>
-    );
+        <Link to="/dashboard" style={{ margin: "20px 0 40px 0" }} class="ui blue huge padded button" >Back</Link>
+      </div>
+    </div>
+  );
 }
 
 export default History;
