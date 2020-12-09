ed341dbcb3a4e81110662870e3f9f57336858650
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2020-12-09 18:40:14 -0500

    Connected signup page

--- client/src/App.js
+++ client/src/App.js
@@ -9,53 +9,49 @@ import Output from './views/Output/Output.js';
 import Settings from "./views/Settings/Settings";
 import History from "./views/History/History";
 import Login from "./views/Login/Login";
+import SignUp from "./views/SignUp/SignUp";
 import './App.css';
 
 
 const App = () => {
-  //Global States
-  const [isAdmin, setIsAdmin] = useState([]);
-  const [isSignedIn, setIsSignedIn] = useState({
-    name: null
-  });
-
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/login" 
-          render={(props)=>(
-            <Login {...props} setIsAdmin={setIsAdmin} setIsSignedIn={setIsSignedIn}/>
-          )}
-        />
-        <Route exact path="/dashboard" 
-          render={(props)=>(
-            <Dashboard {...props} isSignedIn={isSignedIn}/>
-          )}
-        />
-        <Route exact path="/calculator" component={Calculator} />
-        <Route exact path="/history" 
-           render={(props)=>(
-            <History {...props}/>
-          )}
-        />
-        <Route exact path="/settings" 
-          render={(props)=>(
-            <Settings {...props} isAdmin={isAdmin}/>
-          )}
-        />
-        <Route path='/CalculatorOutput' 
-          render={(props)=>(
-            <CalculatorOutput {...props} />
-          )}
-        />
-        <Route exact path="/">
-          <Redirect to="/dashboard" />
-        </Route>
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  );
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/login"
+            render={(props) => (
+              <Login {...props}  />
+            )}
+          />
+          <Route exact path="/dashboard"
+            render={(props) => (
+              <Dashboard {...props} />
+            )}
+          />
+          <Route exact path="/calculator" component={Calculator} />
+          <Route exact path="/history"
+            render={(props) => (
+              <History {...props} />
+            )}
+          />
+          <Route exact path="/settings"
+            render={(props) => (
+              <Settings {...props} />
+            )}
+          />
+          <Route path='/output'
+            render={(props) => (
+              <Output {...props} />
+            )}
+          />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
 }
 
 export default App;
